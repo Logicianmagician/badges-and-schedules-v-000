46219e68ac9ad2d4b9f12392a9cfa2632c8fb2aa
Andrew Kairys <andrewkairys@gmail.com>
2018-03-05 19:19:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(name, room)
-  name.find_index do |name, index|
+  name.map! do |name, index|
     "Hello, #{name}! You'll be assigned to room #{index}!"
   end
 end
