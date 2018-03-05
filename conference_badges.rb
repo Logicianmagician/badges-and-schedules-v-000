9ca10b7b049ce4938a8e9643c0cb4a2e604340cd
Andrew Kairys <andrewkairys@gmail.com>
2018-03-05 18:59:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 index = 1
 def assign_rooms(name, room)
   name.collect.with_index do |name, index|
-    "Hello, #{name}! You'll be assigned to room #{{index}}!"
+    "Hello, #{name}! You'll be assigned to room #{index}!"
      index = index +=1
   end
 end
